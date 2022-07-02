7e6ee707a0f4c9998edf937a1341b3d9765a0a74
MicroFilm7aseb <micro7aseb@gmail.com>
2022-07-02 08:22:40 +0200

    7aseb commit

---
+++ challenge3/src/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="bg-background h-screen">
+      <div className="container  mx-auto ">
+        <div className="grid place-items-center h-screen">
+          <div className="p-10 bg-[#15273F] rounded-lg drop-shadow-md shadow-lg">
+            <img src="" alt="ss" className='h-11 w-10' />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
